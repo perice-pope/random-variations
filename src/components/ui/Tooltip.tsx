22e245ed664ce9d96400b609244938940dab50ac
Dmitry Borody <dmitriy.borodiy@gmail.com>
2018-12-23 16:30:57 +0700

    Tooltips: only show them on large screens because they cause issues on mobile phones

--- src/components/ui/Tooltip.tsx
+++ src/components/ui/Tooltip.tsx
@@ -2,24 +2,26 @@ import * as React from 'react'
 import { css } from 'emotion'
 
 import MuTooltip, { TooltipProps } from '@material-ui/core/Tooltip'
+import withWidth, { WithWidth } from '@material-ui/core/withWidth'
 
-const Tooltip: React.SFC<TooltipProps & { variant?: 'primary' | 'gray' }> = ({
-  variant,
-  ...props
-}) => (
-  <MuTooltip
-    {...props}
-    disableTouchListener
-    disableFocusListener
-    classes={{
-      tooltip: css({
-        fontSize: '1rem',
-        background: variant === 'gray' ? '#888' : '#3f51b5',
-        userSelect: 'none',
-      }),
-    }}
-    {...props}
-  />
-)
+const Tooltip: React.SFC<
+  TooltipProps & WithWidth & { variant?: 'primary' | 'gray' }
+> = ({ variant, width, ...props }) =>
+  width === 'lg' || width === 'xl' || width === 'md' ? (
+    <MuTooltip
+      disableTouchListener
+      disableFocusListener
+      classes={{
+        tooltip: css({
+          fontSize: '1rem',
+          background: variant === 'gray' ? '#888' : '#3f51b5',
+          userSelect: 'none',
+        }),
+      }}
+      {...props}
+    />
+  ) : (
+    props.children
+  )
 
-export default Tooltip
+export default withWidth()(Tooltip)
