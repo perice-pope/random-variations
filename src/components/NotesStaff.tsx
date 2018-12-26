6d5b5d6f1f8062010721acfbad84681a08e70bfc
Dmitry Borody <dmitriy.borodiy@gmail.com>
2018-12-26 21:29:48 +0700

    Notes staff: major improvements in multi-line staves with high/low notes. Now lines adjust their vertical position automatically to avoid overlapping with other staves and chord labels

--- src/components/NotesStaff.tsx
+++ src/components/NotesStaff.tsx
@@ -62,11 +62,11 @@ class NotesStaff extends React.Component<NotesStaffProps, NotesStaffState> {
   private renderContext: Vex.IRenderContext
 
   private boxRef: React.RefObject<any> = React.createRef()
-  private labelAnnotations: Vex.Flow.Modifier[] = []
 
   private notesPerTick: Vex.Flow.Note[][] = []
   private tickToLine: { [k: number]: number } = {}
   private staves: Vex.Flow.Stave[] = []
+  private staveVoices: Vex.Flow.Voice[] = []
   private activeLineEl?: SVGElement
 
   componentDidMount() {
@@ -105,6 +105,11 @@ class NotesStaff extends React.Component<NotesStaffProps, NotesStaffState> {
       this.root,
       Vex.Flow.Renderer.Backends.SVG,
     )
+    const height = this.getHeight()
+    const width = this.state.boxWidth
+
+    // Configure the rendering context
+    this.renderer.resize(width, height)
     this.redraw()
   }
 
@@ -114,29 +119,32 @@ class NotesStaff extends React.Component<NotesStaffProps, NotesStaffState> {
   }
 
   private getHeight = () => {
-    return (
-      (this.props.topOffset + this.getLinesCount() * this.props.staveHeight) *
-      this.props.scale!
-    )
+    if (!this.staveVoices || this.staveVoices.length === 0) {
+      return (
+        (this.props.topOffset + this.getLinesCount() * this.props.staveHeight) *
+        this.props.scale!
+      )
+    } else {
+      const lastStaveBB = this.staveVoices[
+        this.staveVoices.length - 1
+      ].getBoundingBox()
+      // @ts-ignore
+      const lastVoiceBB = this.staves[this.staves.length - 1].getBoundingBox()
+
+      return (
+        (20 +
+          Math.max(
+            lastStaveBB.getY() + lastStaveBB.getH(),
+            lastVoiceBB.getY() + lastVoiceBB.getH(),
+          )) *
+        this.props.scale!
+      )
+    }
   }
 
   private redraw = () => {
     console.log('NotesStaff -> redraw')
-    const height = this.getHeight()
-    const width = this.state.boxWidth
-
-    // Configure the rendering context
-    this.renderer.resize(width, height)
     this.renderContext = this.renderer.getContext()
-    this.renderContext.scale(this.props.scale || 1, this.props.scale || 1)
-
-    this.renderContext.clear()
-    this.renderContext
-      .setFont('Arial', 14)
-      .setBackgroundFillStyle('white')
-      .setFillStyle('black')
-      .setStrokeStyle('black')
-    this.renderContext.save()
 
     this.drawStaveAndClef()
     this.drawActiveNoteLine()
@@ -278,9 +286,9 @@ class NotesStaff extends React.Component<NotesStaffProps, NotesStaffState> {
     }
   }
 
-  private drawStaveAndClef = () => {
+  private drawStaveAndClef = (staveYs?: number[]) => {
     console.log('NotesStaff -> drawStaveAndClef')
-    const { staveHeight, scale, topOffset } = this.props
+    const { staveHeight, scale } = this.props
     const linesCount = this.getLinesCount()
     const width = this.state.boxWidth / (scale || 1.0)
 
@@ -289,7 +297,7 @@ class NotesStaff extends React.Component<NotesStaffProps, NotesStaffState> {
     for (let i = 0; i < linesCount; ++i) {
       this.staves[i] = new Vex.Flow.Stave(
         0,
-        topOffset - 10 + staveHeight * i,
+        staveYs && !!staveYs[i] ? staveYs[i] : staveHeight * i,
         width,
       )
       if (i === 0) {
@@ -302,14 +310,14 @@ class NotesStaff extends React.Component<NotesStaffProps, NotesStaffState> {
       }
 
       // Connect it to the rendering context and draw!
-      this.staves[i].setContext(this.renderContext).draw()
+      this.staves[i].setContext(this.renderContext)
     }
   }
 
   private drawNotes = () => {
     console.log('NotesStaff -> drawNotes')
     const ticks = this.getTicksTransformed()
-    const { staves, renderContext } = this
+    const { renderContext } = this
     const linesCount = this.getLinesCount()
 
     // Clear the old notes
@@ -322,13 +330,13 @@ class NotesStaff extends React.Component<NotesStaffProps, NotesStaffState> {
     // @ts-ignore
     // renderContext.svg.querySelectorAll('.rect').forEach(n => n.remove())
 
-    if (!staves || !ticks || ticks.length === 0) {
+    if (!this.staves || !ticks) {
       return
     }
 
     let tickIndexToCardId = {}
 
-    this.labelAnnotations = []
+    // Create VexFlow notes based on the `props.ticks`
     const tickToNotes = ticks.map((tick, index) => {
       const shouldAddMeasureLine =
         index < ticks.length - 1 &&
@@ -421,15 +429,17 @@ class NotesStaff extends React.Component<NotesStaffProps, NotesStaffState> {
     const cardIdToTickIndexes = _.groupBy(tickIndexes, i => ticks[i].noteCardId)
     const cardIds = _.keys(cardIdToTickIndexes)
 
+    // Map note cards to staff lines
     const cardIdToLine = this.getCardsToLinesMapping()
     const lineToCardIds = _.groupBy(cardIds, cardId => cardIdToLine[cardId])
 
+    // Map staff ticks to lines
     this.tickToLine = {}
     tickToNotes.forEach((tick, tickIndex) => {
       this.tickToLine[tickIndex] = cardIdToLine[tickIndexToCardId[tickIndex]]
     })
 
-    const lineToTickNotes = {}
+    const lineToTickNotes: { [k: number]: Vex.Flow.Note[][] } = {}
     for (let i = 0; i < linesCount; ++i) {
       lineToTickNotes[i] = []
       if (!lineToCardIds[i]) {
@@ -447,57 +457,146 @@ class NotesStaff extends React.Component<NotesStaffProps, NotesStaffState> {
       })
     }
 
+    const lineFormatters = {}
+    const lineVoices = {}
+    this.staveVoices = []
+
+    // Do the initial notes formatting
     for (let i = 0; i < linesCount; ++i) {
-      Vex.Flow.Formatter.FormatAndDraw(
-        this.renderContext,
-        staves[i],
-        _.flatten(lineToTickNotes[i]),
-      )
+      const notes = _.flatten(lineToTickNotes[i]) as Vex.Flow.Tickable[]
+      if (notes.length === 0) {
+        continue
+      }
+
+      const voice = new Vex.Flow.Voice(Vex.Flow.TIME4_4)
+        .setMode(Vex.Flow.Voice.Mode.SOFT)
+        .setStave(this.staves[i])
+        .addTickables(notes)
+
+      this.staveVoices[i] = voice
+      lineVoices[i] = voice
+
+      const formatter = new Vex.Flow.Formatter()
+
+      formatter.joinVoices([voice]).formatToStave([voice], this.staves[i], {
+        context: this.renderContext,
+      })
+
+      lineFormatters[i] = formatter
     }
 
-    tickToNotes.forEach((notes, tickIndex) => {
-      if (
-        notes.length === 0 ||
-        !this.props.tickLabels ||
-        !this.props.tickLabels[tickIndex]
-      ) {
-        return
+    // Adjust heights and vertical position of staves so that text labels and notes don't overlap
+    // with other notes / staves
+
+    let totalDiffs = 0
+    let endOfPreviousLine = this.props.topOffset
+
+    this.drawStaveAndClef()
+    // @ts-ignore
+    const staveYs: number[] = this.staves.map(s => s.getBoundingBox().getY())
+    try {
+      for (let i = 0; i < linesCount; ++i) {
+        const stave = this.staves[i]
+        const voice = lineVoices[i] as Vex.Flow.Voice
+
+        // @ts-ignore
+        const staveBB = stave.getBoundingBox()
+        const voiceBB = voice.getBoundingBox()
+
+        const endOfStave = staveBB.getY() + staveBB.getH()
+        const endOfVoice = voiceBB.getY() + voiceBB.getH()
+
+        const startOfLine = Math.min(staveBB.getY(), voiceBB.getY())
+        const endOfLine = Math.max(endOfStave, endOfVoice) + 40
+
+        staveYs[i] = staveBB.getY()
+
+        if (startOfLine < endOfPreviousLine) {
+          const yDiff = endOfPreviousLine - startOfLine
+          staveYs[i] += yDiff + totalDiffs
+          totalDiffs += yDiff
+        }
+
+        endOfPreviousLine = endOfLine
+      }
+    } catch (error) {
+      console.error('Could not adjust vertical positions of staves', error)
+    }
+
+    this.drawStaveAndClef(staveYs)
+
+    // Finally, render the formatted notes
+    for (let i = 0; i < linesCount; ++i) {
+      this.staves[i].setContext(this.renderContext).draw()
+
+      if (lineFormatters[i]) {
+        lineFormatters[i].formatToStave([lineVoices[i]], this.staves[i], {
+          context: this.renderContext,
+        })
       }
 
-      const label = this.props.tickLabels[tickIndex]
-      let fontSize = 17
-      if (label.length > 2) {
-        fontSize = 15
+      if (lineVoices[i]) {
+        lineVoices[i].draw(this.renderContext, this.staves[i])
       }
-      if (label.length > 4) {
-        fontSize = 14
-      }
-      if (label.length > 6) {
-        fontSize = 13
-      }
-      if (label.length > 8) {
-        fontSize = 12
-      }
+    }
 
-      const firstNote = notes[0]
-      const y = firstNote.getYForTopText(1)
-      const x = firstNote.getAbsoluteX()
+    setTimeout(() => {
+      const height = this.getHeight()
+      const width = this.state.boxWidth
+
+      console.log('height: ', height, width)
+
+      // Configure the rendering context
+      this.renderer.resize(width, height)
+
+      this.renderContext.scale(this.props.scale || 1, this.props.scale || 1)
 
       this.renderContext
-        .setFont('Sans-Serif', fontSize, 900)
-        .fillText(label, x, y)
-    })
+        .setFont('Arial', 14)
+        .setBackgroundFillStyle('white')
+        .setFillStyle('black')
+        .setStrokeStyle('black')
+      this.renderContext.save()
 
-    this.renderContext.setStrokeStyle('black')
-    this.renderContext.setFillStyle('black')
-    this.labelAnnotations.forEach(modifier => {
-      modifier.draw()
-    })
+      // Render text labels
+      tickToNotes.forEach((notes, tickIndex) => {
+        if (
+          notes.length === 0 ||
+          !this.props.tickLabels ||
+          !this.props.tickLabels[tickIndex]
+        ) {
+          return
+        }
 
-    this.notesPerTick = tickToNotes
+        const label = this.props.tickLabels[tickIndex]
+        let fontSize = 17
+        if (label.length > 2) {
+          fontSize = 15
+        }
+        if (label.length > 4) {
+          fontSize = 14
+        }
+        if (label.length > 6) {
+          fontSize = 13
+        }
+        if (label.length > 8) {
+          fontSize = 12
+        }
 
-    this.drawActiveNoteLine()
-    this.updateActiveNoteLine()
+        const firstNote = notes[0]
+        const y = firstNote.getYForTopText(1)
+        const x = firstNote.getAbsoluteX()
+
+        this.renderContext
+          .setFont('Sans-Serif', fontSize, 900)
+          .fillText(label, x, y)
+      })
+
+      this.notesPerTick = tickToNotes
+
+      this.drawActiveNoteLine()
+      this.updateActiveNoteLine()
+    }, 0)
   }
 
   scrollToDebounced = _.debounce(
@@ -523,7 +622,7 @@ class NotesStaff extends React.Component<NotesStaffProps, NotesStaffState> {
     if (!isPlaying) {
       return
     }
-    const { staveHeight } = this.props
+
     const ticks = this.getTicksTransformed()
     const { notesPerTick } = this
 
@@ -543,20 +642,21 @@ class NotesStaff extends React.Component<NotesStaffProps, NotesStaffState> {
           (activeNote.getNoteHeadEndX() - activeNote.getNoteHeadBeginX()) / 2
 
         const activeLineXNew = noteHeadX
-        const activeLineYNew = this.tickToLine[activeTickIndex] * staveHeight
+        const activeLineYNew =
+          this.staves[this.tickToLine[activeTickIndex]]
+            // @ts-ignore
+            .getBoundingBox()
+            // @ts-ignore
+            .getY() - this.staves[0].getBoundingBox().getY()
 
-        this.scrollToDebounced(activeLineYNew * (this.props.scale || 1.0))
+        this.scrollToDebounced(
+          (activeLineYNew - 40) * (this.props.scale || 1.0),
+        )
 
         // @ts-ignore
         this.activeLineEl.style = `transform: translateX(${activeLineXNew}px) translateY(${activeLineYNew}px);`
 
         activeNote.draw()
-
-        this.renderContext.setStrokeStyle('black')
-        this.renderContext.setFillStyle('black')
-        this.labelAnnotations.forEach(modifier => {
-          modifier.draw()
-        })
       }
     }
   }
